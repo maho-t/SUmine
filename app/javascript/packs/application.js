0c66aafbcab25633a3cbda599018c85ee9ae72a7
pipimaho <mtateno0@gmail.com>
2022-06-13 18:25:33 +0900

    トップ画面に戻るボタンの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("../side_bar")
 require("../user_add")
+require("../application")
 import "bootstrap"
 import "../stylesheets/application"
 
