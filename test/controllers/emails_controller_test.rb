3b8ec95bdefa22c34d0ae55d9721d5d69519ffbe
pipimaho <mtateno0@gmail.com>
2022-06-19 15:30:49 +0900

    ファイルの名称変更に伴う修正

--- test/controllers/emails_controller_test.rb
+++ test/controllers/emails_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MailsControllerTest < ActionDispatch::IntegrationTest
+class EmailsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
