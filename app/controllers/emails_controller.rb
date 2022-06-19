b5a14944e5b4be9e73c07285f1d3dc6f8bbdd005
pipimaho <mtateno0@gmail.com>
2022-06-20 08:06:56 +0900

    メール管理一覧画面と新規作成のコントローラー実装

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,34 @@
 class EmailsController < ApplicationController
+  before_action :find_team, only: [:choose, :new, :create]
 
   def index
     @teams = Team.all
+  end
+
+  def choose
+    @emails = @team.emails.includes(:user).order("created_at DESC").page(params[:page]).per(20)
+  end
+
+  def new
+    @emails = Email.new
+  end
+
+  def create
+    email = @team.emails.new(email_params)
+    if email.save
+      redirect_to choose_team_emails_path(@team)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:title, :text, :shiryo).merge(user_id: current_user.id)
+  end
+  
+  def find_team
+    @team = Team.find(params[:team_id])
   end
 end
