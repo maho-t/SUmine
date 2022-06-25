c4275b7e5a5816f6435c113ca3c3d4d55b7230e8
pipimaho <mtateno0@gmail.com>
2022-06-25 15:56:47 +0900

    メンバー追加の表示変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("../side_bar")
-require("../user_add")
 require("../application")
 import "bootstrap"
 import "../stylesheets/application"
--- app/javascript/user_add.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  // 新規作成・編集ページのフォームを取得
-
-  
-})
