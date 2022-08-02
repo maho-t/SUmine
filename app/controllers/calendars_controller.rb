4b2a436a496ea4dc934ebf62e6a3e0e576e387d3
pipimaho <mtateno0@gmail.com>
2022-08-02 11:23:44 +0900

    コントローラー実装

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,23 @@
 class CalendarsController < ApplicationController
 
   def index
+    @calendars = Calendar.all
+    @calendar = Calendar.new
+  end
+  
+  def new
+    @calendar = Calendar.new
+  end
+
+  def create
+    Calendar.create(calendar_params)
+    redirect_to team_calendars_path
+  end
+
+  private
+
+  def calendar_params
+    params.require(:calendar).permit(:title)
   end
   
 end
